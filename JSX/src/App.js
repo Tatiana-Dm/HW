b684e4b7b87af77a14fc6528bc72e2aec80d8311
Tatiana-Dm <83395904+Tatiana-Dm@users.noreply.github.com>
2021-09-27 20:11:54 +0300

    Add files via upload

---
+++ JSX/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { MyComponents } from "./components/MyComponents";
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponents.Users />
+    </div>
+  );
+}
+
+export default App;
