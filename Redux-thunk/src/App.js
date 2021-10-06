2b5b37672566384faedd5f5635d6ebd81c86bc31
Tatiana-Dm <83395904+Tatiana-Dm@users.noreply.github.com>
2021-10-06 12:32:48 +0300

    Add files via upload

---
+++ Redux-thunk/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import { PostsContainer } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+        <PostsContainer />
+    </div>
+  );
+}
+
+export default App;
